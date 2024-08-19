ee18345bb35f22b2ad95bdb937c078c78ce74d26
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-19 17:04:53 -0400

    Update

---
+++ Proyecto2_003/src/Pizza.java
@@ -0,0 +1,17 @@
+
+public class Pizza {
+    // atributos
+    private String nombre;
+    private String tamano;
+    private String masa;
+    private int precio;
+    // constructores
+    
+    // getter y setter
+    public void setNombre(String nombre)
+    {
+        nombre = nombre;
+    }
+    // metodos personalizados
+    
+}
---
+++ Proyecto2_003/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+    
+    // psvm + tab
+    // main + tab
+    public static void main(String[] args) {
+        
+        Pizza pizza1 = new Pizza();
+        
+
+    }
+}

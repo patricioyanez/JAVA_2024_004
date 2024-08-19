bf51b54f7dc55328c10b61e92534fce631b4c4ae
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-19 12:08:47 -0400

    clase

---
+++ Proyecto2_004/src/Pizza.java
@@ -0,0 +1,29 @@
+
+public class Pizza {
+    // atributos
+    private String nombre;
+    private String masa;
+    private String tamano;
+    private int precio;
+    
+    // constructor
+    
+    
+    // getter and setter
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    // crear g and s de los otros atributos
+    
+    // metodos personalizados
+    public void calcular()
+    {
+        precio = 11000;
+    }
+}

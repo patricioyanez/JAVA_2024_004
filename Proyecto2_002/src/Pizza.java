ed5c32532e0c2ec6933d0964ee55138cffba2f05
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-20 14:44:06 -0400

    update

--- Proyecto2_002/src/Pizza.java
+++ Proyecto2_002/src/Pizza.java
@@ -1,13 +1,51 @@
 
 public class Pizza {
     // atributos
+    private String nombre;
+    private String tamano;
+    private String masa;
+    private int precio;
     
     // constructor
     
     // getter and setter
+    public String getNombre()
+    {
+        return nombre;
+    }
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    // Ejercicio 2: realizar gettter y setter
+    // de los otros atributos
+    
+    
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getMasa() {
+        return masa;
+    }
+
+    public void setMasa(String masa) {
+        this.masa = masa;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
     
     
     // metodos personalizados
-    
-    
+
 }
--- Proyecto2_002/src/Principal.java
+++ Proyecto2_002/src/Principal.java
@@ -4,6 +4,20 @@ public class Principal {
     // main + tab
     public static void main(String[] args) {
         
+        // crea un objeto
+        Pizza pizza1 = new Pizza();
+        // asignar valor a cada atributo
+        pizza1.setNombre("Vegetaria");
+        pizza1.setMasa("Delgada");
+        pizza1.setTamano("Familiar");
+        pizza1.setPrecio(12000);
+        // muestra información de cada atributo del objeto
+        System.out.println("== Datos Pizza == ");
+        System.out.println("Nombre  : " + pizza1.getNombre());
+        System.out.println("Masa    : " + pizza1.getMasa());
+        System.out.println("Tamaño  : " + pizza1.getTamano());
+        System.out.println("Precio  : " + pizza1.getPrecio());
+        
     }
     
 }

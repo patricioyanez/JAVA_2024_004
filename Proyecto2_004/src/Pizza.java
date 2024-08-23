d900f473c309a97e76fe9ff9539e97234acf8705
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-23 09:41:30 -0400

    update

--- Proyecto2_004/src/Pizza.java
+++ Proyecto2_004/src/Pizza.java
@@ -20,10 +20,43 @@ public class Pizza {
         return this.nombre;
     }
     // crear g and s de los otros atributos
+
+    public String getMasa() {
+        return masa;
+    }
+
+    public void setMasa(String masa) {
+        this.masa = masa;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
     
     // metodos personalizados
     public void calcular()
     {
         precio = 11000;
+    }
+    
+    public void imprimirDatos()
+    {
+        System.out.println("== Datos Pizza ==");
+        System.out.println("Nombre  : " + getNombre());
+        System.out.println("Masa    : " + getMasa());
+        System.out.println("Tamano  : " + getTamano());
+        System.out.println("Precio  : " + precio);
     }
 }
--- Proyecto2_004/src/Principal.java
+++ Proyecto2_004/src/Principal.java
@@ -3,7 +3,38 @@ public class Principal {
     // psvm + tab
     // main + tab
     public static void main(String[] args) {
+        // creacion de objeto
         Pizza pizza1 = new Pizza();
+        
+        // ingresar datos al objeto
+        pizza1.setNombre("Vegetaria");
+        pizza1.setMasa("Delgada");
+        pizza1.setTamano("Media");
+        pizza1.setPrecio(10000);
 
+        // mostrar los datos del objeto
+        System.out.println("== Datos Pizza ==");
+        System.out.println("Nombre  : " + pizza1.getNombre());
+        System.out.println("Masa    : " + pizza1.getMasa());
+        System.out.println("Tamano  : " + pizza1.getTamano());
+        System.out.println("Precio  : " + pizza1.getPrecio());
+        
+        // Ejercicio:
+        // Crear una segunda pizza y mostrar los datos
+        
+        Pizza pizza2 = new Pizza();
+        
+        // ingresar datos al objeto
+        pizza2.setNombre("Hawaina");
+        pizza2.setMasa("Normal");
+        pizza2.setTamano("Familiar");
+        pizza2.setPrecio(15000);
+
+        // mostrar los datos del objeto
+        System.out.println("== Datos Pizza ==");
+        System.out.println("Nombre  : " + pizza2.getNombre());
+        System.out.println("Masa    : " + pizza2.getMasa());
+        System.out.println("Tamano  : " + pizza2.getTamano());
+        System.out.println("Precio  : " + pizza2.getPrecio());
     }
 }

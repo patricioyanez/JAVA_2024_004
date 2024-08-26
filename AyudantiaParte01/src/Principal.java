8a5632ef8ed9cc4a97dd0ba9489b97b2c7e76049
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-26 12:07:27 -0400

    update

---
+++ AyudantiaParte01/src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente();
+        Cliente c2 = new Cliente("1-9", "juan", "Perez", "Vespucio 1501", "juan@duoc.cl", 25);
+        
+    }
+    
+}
---
+++ Proyecto2_004/src/PrincipalSentencias.java
@@ -0,0 +1,93 @@
+
+import java.util.Scanner;
+
+
+
+public class PrincipalSentencias {
+    public static void main(String[] args) {
+        int numero = 0;
+        
+        if(numero == 0)
+            System.out.println("El valor es cero");
+        
+        if(numero >= 0)
+        {
+            System.out.println("Es Verdadero");
+            System.out.println("Numero no es negativo");
+        }
+        
+        if(numero > 0)
+        {
+            System.out.println("if/else: verdadero");
+        }
+        else
+        {
+            System.out.println("if/else: falso");
+        }
+        
+        int numero2 = 10;
+        
+        if(numero2 == 0)
+        {
+            System.out.println("Numero es cero");
+        }
+        else if(numero2 > 0)
+        {
+            System.out.println("Numero 2 es mayor a cero");
+        }
+        
+        // if/ else if /else
+        if(numero2 == 0)
+        {
+            System.out.println("Numero es cero");
+        }
+        else if(numero2 > 0)
+        {
+            System.out.println("Numero 2 es mayor a cero");
+        }
+        else
+        {
+            System.out.println("Todo es falso");
+        }
+        
+        // Ejercicio:
+        // preguntar al usuario un numero y enviar 
+        // si es natural o no
+        Scanner leerTeclado = new Scanner(System.in);
+        System.out.print("Ingrese un numero: ");
+        int numero1 = leerTeclado.nextInt();
+        
+        if(numero1 > 0)
+            System.out.println("Es numero natural");
+        else
+            System.out.println("El numero no es natural");
+        
+        
+        // determinar si el numero es +, - o neutro
+        
+        if(numero1 > 0)
+            System.out.println("El numero es positivo");
+        else if(numero1 == 0)
+            System.out.println("El numero es neutro");
+        else
+            System.out.println("El Numero es negativo");
+        
+        // solicitar la edad y determinar el rango etario
+        // niño, adolescente, adulto y 3ra edad
+        System.out.print("Ingrese su edad: ");
+        int edad = leerTeclado.nextInt();
+        if (edad <= 11) {
+            System.out.println("Es niño");
+        }
+        else if (edad < 18) {
+            System.out.println("Adolescente");
+        }
+        else if (edad < 65) {
+            System.out.println("Adulto");
+        }
+        else
+        {
+            System.out.println("3ra Edad");
+        }
+    }
+}

ba8f932d2ccda6e18146fe223021df99b7888ea6
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-28 09:50:52 -0400

    update

--- Ayudantia/Ayudantia004_Parte01/src/Principal.java
+++ Ayudantia/Ayudantia004_Parte01/src/Principal.java
@@ -2,7 +2,20 @@
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Componente procesador = new Componente(10, "intel", "i9", "Procesador 7ta G", 150000);
+        Componente memoria = new Componente(12, "Kingtons", "ddr5", "Memoria 64GB", 120000);
+        Componente ssd = new Componente(15, "WD", "dfgsdf", "SSD 960GB", 100000);
+        
+        Cliente cliente1 = new Cliente("1-9", "Juan", "Diaz", "Vespucio 1501", "juan@mail.com", 30);
+        Cliente cliente2 = new Cliente("2-7", "Ana", "Aros", "Vespucio 1703", "ana@mail.com", 35);
+        
+        System.out.println("Listado de componentes");
+        System.out.println(procesador.imprimirDatos());
+        System.out.println(memoria.imprimirDatos());
+        System.out.println(ssd.imprimirDatos());
+        System.out.println("Listado de Clientes");
+        System.out.println(cliente1.imprimirDatos());
+        System.out.println(cliente2.imprimirDatos());
     }
     
 }

3de6823265899f8de5c6899d7203ca4254178c74
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-09 13:54:47 -0300

    update

--- EjemploEvaluacion004/src/Principal.java
+++ EjemploEvaluacion004/src/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
         Cliente cliente1 = new Cliente(100, "juan", 30, 546540, "asdfasdfq@fasdfa.cl", true);
         
         Vehiculo v1 = new Vehiculo("fasdf", "Honda", 10, 50000, cliente1);
+        
         System.out.println(v1.getCliente().imprimirDatos());
         
     }

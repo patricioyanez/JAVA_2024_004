4a9643cc6de82070ef68b754cb72679e9ec7ca76
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-11 09:01:07 -0300

    update

--- EjemploEvaluacion004/src/Principal.java
+++ EjemploEvaluacion004/src/Principal.java
@@ -1,13 +1,13 @@
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
-        
-        Cliente cliente1 = new Cliente(100, "juan", 30, 546540, "asdfasdfq@fasdfa.cl", true);
-        
-        Vehiculo v1 = new Vehiculo("fasdf", "Honda", 10, 50000, cliente1);
-        
-        System.out.println(v1.getCliente().imprimirDatos());
+        Scanner leerTeclado = new Scanner(System.in);
+        int opcion = 0;
+        Vehiculo vehiculo = new Vehiculo();
+        Cliente cliente = new Cliente();
         
     }
     

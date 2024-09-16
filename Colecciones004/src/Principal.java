7018728a4e416d787b55314599327c13ffb5625d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-16 12:05:42 -0300

    update

---
+++ Colecciones004/src/Principal.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // crear coleccion arrayList para String
+        ArrayList<String> nombres = new ArrayList<String>();
+        
+        // agregar elementos
+        nombres.add("Francisca");
+        nombres.add("Juan");
+        nombres.add("Ana");
+        nombres.add("Diego");
+        
+        int con = 0;
+        for(String auxiliar : nombres)
+        {
+            con++;
+            System.out.println("Nombre " + con + " : " + auxiliar);
+        }
+        
+        // buscar
+        boolean fueEncontrado = false;
+        fueEncontrado = nombres.contains("Juan");
+        if(fueEncontrado)
+            System.out.println("Encontrado");
+        else
+            System.out.println("No encontrado");
+        
+        // mostrar elemento según indice
+        System.out.println("indice 3: " + nombres.get(2) );
+       
+        // indice del elemento buscado
+        nombres.add("Ana");
+        int indice = nombres.indexOf("Adam");
+        System.out.println("Indice 1ra coincidencia: " + indice);
+        
+        for(String aux: nombres)
+        {
+            System.out.println("===> " + aux);
+        }
+        // quitar elemento
+        if(nombres.remove("Juan"))
+            System.out.println("Juan fue Eliminado");
+        else
+            System.out.println("No fue eliminado");
+        
+        for(String aux: nombres)
+        {
+            System.out.println("===> " + aux);
+        }
+        
+        // devuelve la cantidad de elementos de la colección
+        System.out.println("Elementos: " + nombres.size());
+    }
+    
+    // Ejercicio: crear una clase producto con 5 atributos
+    // y agregar elementos a una colección.
+    
+    
+}

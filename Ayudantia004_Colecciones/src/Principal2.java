1230ab4d16ef3795c5ddbde3b06003942161b90b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-25 09:20:42 -0300

    update

--- Ayudantia004_Colecciones/src/Principal2.java
+++ Ayudantia004_Colecciones/src/Principal2.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Crear un menu que permita ingresar nombres y apellidos
 // y sus notas (1 por persona) y otra para listarlos.
 // Solicitar los datos por teclado.
 
-
 public class Principal2 {
     public static void main(String[] args) {
+        ArrayList<String> nombres = new ArrayList<String>();
+        ArrayList<Integer> notas = new ArrayList<Integer>();
+        Scanner leerTeclado = new Scanner(System.in);
+        int opcion = 0;
+        
+        while(true)
+        {
+            System.out.println("==== Menú ====");
+            System.out.println("1.-Ingresar datos");
+            System.out.println("2.-Listar");
+            System.out.println("3.-Salir");
+            System.out.print("Ingrese opción: ");
+            opcion = leerTeclado.nextInt();
+        }
         
     }
 }

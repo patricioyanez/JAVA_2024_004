54258e33da0112dd752cc68a251e7a36d84c9fb1
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-25 09:29:50 -0300

    update

--- EjercicioTipoPrueba2_004/src/Principal.java
+++ EjercicioTipoPrueba2_004/src/Principal.java
@@ -1,20 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author patri
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Auto auto1 = new Auto(5, 150, "qwer12", "Ana", 20000, 2380, 22380);
+        Moto moto1 = new Moto("sPort", 50, "qw9865", "Diego", 10000, 1190, 11900);
+        Moto moto2 = new Moto("sPort", 50, "qw9867", "Luis", 10000, 1190, 11900);
+        
+        Taller taller = new Taller();
+        taller.agregar(auto1);
+        taller.agregar(moto1);
+        taller.agregar(moto2);
+        
+        auto1.obtenerDescuento();
+        auto1.totalConIVA();
+        auto1.totalPorPagar();
+        
+        System.out.println(auto1);
+        
+        System.out.println("==== Listado de Vehiculos ====");
+        taller.listar();
+        
+        System.out.println("");
+        taller.contabilizar();
+        
+        
     }
     
 }

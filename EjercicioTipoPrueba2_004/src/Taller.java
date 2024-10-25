582b0b8767040ed5a00d18e1e1caf3f097d3241a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-25 09:19:46 -0300

    update

--- EjercicioTipoPrueba2_004/src/Taller.java
+++ EjercicioTipoPrueba2_004/src/Taller.java
@@ -1,13 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.ArrayList;
 
-/**
- *
- * @author patri
- */
 public class Taller {
+    private ArrayList<Vehiculo> listado = new ArrayList<Vehiculo>();
     
+    
+    public boolean agregar(Vehiculo vehiculo)    
+    {
+        if(this.buscarPorPatente(vehiculo.getPatente()))
+            return false;
+        return listado.add(vehiculo);
+    }
+    
+    public boolean buscarPorPatente(String patente)
+    {
+        for (Vehiculo vehiculo : listado) {
+            if(vehiculo.getPatente().equalsIgnoreCase(patente))
+                return true;
+        }
+        return false;
+    }
+    public void listar()
+    {
+        for (Vehiculo vehiculo : listado) {
+            System.out.println(vehiculo);
+        }
+    }
+    public void contabilizar()
+    {
+        int auto = 0;
+        int moto = 0;
+        for (Vehiculo vehiculo : listado) {
+            if(vehiculo instanceof Auto)
+                auto++;
+            else
+                moto++;
+        }
+        System.out.println("Contabilidar vehiculos");
+        System.out.println("Auto:" + auto);
+        System.out.println("Moto:" + moto);
+    }
 }

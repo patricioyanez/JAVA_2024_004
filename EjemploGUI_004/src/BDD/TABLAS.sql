a5b69e508120299dbab27b27c30906ca14215aec
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-11-08 09:23:19 -0300

    update

--- EjemploGUI_004/src/BDD/TABLAS.sql
+++ EjemploGUI_004/src/BDD/TABLAS.sql
@@ -4,3 +4,9 @@ CREATE TABLE MARCA(
     habilitado  int             not null,
     unique(nombre)
 );
+
+INSERT INTO Marca (nombre, habilitado) values ('CocaCola', 1);
+INSERT INTO Marca (nombre, habilitado) values ('Pepsi', 1);
+INSERT INTO Marca (nombre, habilitado) values ('Bilz', 1);
+INSERT INTO Marca (nombre, habilitado) values ('Pap', 1);
+INSERT INTO Marca (nombre, habilitado) values ('RedBull', 1);

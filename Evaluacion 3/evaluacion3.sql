c8d33c6220367fce0abbd4faa2887e47b9c66884
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-11-24 18:29:43 -0300

    update

---
+++ Evaluacion 3/evaluacion3.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Sello
+(
+    idSello             int             primary key auto_increment,
+    nombre              varchar(50)     not null,
+    UNIQUE(nombre)
+);
+
+CREATE TABLE Cargo
+(
+    idCargo             int             primary key auto_increment,
+    nombre              varchar(50)     not null,
+    UNIQUE(nombre)
+);
+
+
+CREATE TABLE Persona
+(
+    idPersona           int             primary key auto_increment,
+    idCargo             int             null,
+    rut                 int             not null,
+    digito              varchar(1)      not null,
+    nombre              varchar(50)     not null,
+    apellido            varchar(50)     not null,
+	direccion			varchar(100)	not null,
+    sueldo              int             null,
+	esCliente			int 			not null,
+	habilitado			int 			not null,
+    UNIQUE(rut),
+    FOREIGN KEY(idCargo)
+                    REFERENCES Cargo(idCargo)
+);
+
+
+CREATE TABLE Produccion
+(
+    idProduccion        int             primary key auto_increment,
+    idSello           	int             not null,
+    nombre             	varchar(100)	not null,
+    anioEstreno      	int             not null,
+    formato             varchar(100)    not null,
+    precio      		int             not null,
+    
+	temporadas       	int             null, -- serie
+	capitulos       	int             null, -- serie
+	duracion       		int             null, -- serie y pelicula
+    clasificacion       varchar(100)    null, -- pelicula
+    UNIQUE(nombre),
+
+    FOREIGN KEY(idSello)
+                    REFERENCES Sello(idSello)
+);
